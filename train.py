e54540b4501a0e1964e64d69dd1bb011273ee9e7
csm-kr <csm8167@naver.com>
2019-08-13 12:31:00 +0900

    fix train

--- train.py
+++ train.py
@@ -4,7 +4,8 @@ import torch.utils.data as data
 from dataset import voc_loader as loader
 import model
 import torch.optim as optim
-
+from loss import MultiBoxLoss
+from model import SSD
 
 # 1. device config
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -24,18 +25,18 @@ train_set = loader.VOC_loader(transform=transform)
 train_loader = data.DataLoader(train_set, batch_size=1, shuffle=True, num_workers=0)
 
 # 5. model 정의
-net = model.RPN().to(device)
+net = SSD().to(device)
 
 # 6. loss 정의
-
+criterion = MultiBoxLoss().to(device)
 
 # 7. optimizer 정의
-optimizer = optim.Adam(net.parameters(), lr=0.0001)
+optimizer = optim.Adam(net.parameters(), lr=0.001)
 
 # 9. train
 for epoch in range(10):
-    for i, (images, labels, _) in enumerate(train_loader):
-        images = images.cuda()
-        labels = labels.cuda()
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
 
         bbox, cls, f = net(images)
